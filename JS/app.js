5178bb5775ea1a319c9e27978fbcc256f739c76a
Sharon Miller <sharon@sharonmillercreative.com>
2018-06-26 12:52:47 -0700

    got images displaying randomly

--- JS/app.js
+++ JS/app.js
@@ -1,58 +1,106 @@
 'use strict';
 //global variables
-var itemImage1 = document.getElementById('item1');
-var itemImage2 = document.getElementById('item2');
-var itemImage3 = document.getElementById('item3');
-var imageSection = document.getElementById('vote');
+var product1 = document.getElementById('product1');
+var prodName1 = document.getElementById('vote1');
+var product2 = document.getElementById('product2');
+var prodName2 = document.getElementById('vote2');
+var product3 = document.getElementById('product3');
+var prodName3 = document.getElementById('vote3');
+var imageSection = document.getElementById('clickMe');
+
 //===================
 
 //create array for all images
-ItemImage.imageArray = [];
+Product.productsArray = [];
 
 //array for images clicked
-ItemImage.totalClickes = 0;
+Product.totalClickes = 0;
 
 //track which items were lastshonwn
 
-function ItemImage(src, imageName) {
+function Product(src, imageName, size) {
   this.src = src;
   this.imageName = imageName;
-  this.imageClickedCount = 0;
+  this.votedFor = 0;
   this.imageDisplayedCount = 0;
-  // this.size = size || 300px;
-  ItemImage.imageArray.push(this);
+  this.size = size || '300px';
+  Product.productsArray.push(this);
 }
-
+Product.voteCount = 0;
 // ItemImage.prototype.render = function(){
 //   console.log ('rendering');
 // };
 
 //rank photos
-ItemImage.rankImages = function() {
-  for(var i in this.imageArray){
-    console.log(this.imageArray[i].imageClickedCount);
+Product.rankImages = function () {
+  for (var i in this.productsArray) {
+    console.log(this.productsArray[i].votedFor);
   }
 };
-var pastLikedItem1 = 0;
-var pastLikedItem2 = 10;
-var pastLikedItem3 = 16;
+//setting up the first three product images as the past displayed images
+var pastRandomNumber1 = 0;
+var pastRandomNumber2 = 1;
+var pastRandomNumber3 = 2;
 
-ItemImage.renderThreeRandomly = function (){
+//funtion to choose three new random images
+Product.renderThreeRandomly = function () {
+  var randomNumber1;
+  var randomNumber2;
+  var randomNumber3;
   do {
-    var randomNumber1 = Math.floor(Math.random() * ItemImage.imageArray.length);
-  } while (randomNumber1 === pastLikedItem1 || randomNumber1 === pastLikedItem2 || randomNumber1 === pastLikedItem3);
+    randomNumber1 = Math.floor(Math.random() * Product.productsArray.length);
+  } while (randomNumber1 === pastRandomNumber1 || randomNumber1 === pastRandomNumber2 || randomNumber1 === pastRandomNumber3);
+  
+  do {
+    randomNumber2 = Math.floor(Math.random() * Product.productsArray.length);
+  } while (randomNumber1 === randomNumber2 || randomNumber2 === pastRandomNumber2 || randomNumber1 === pastRandomNumber1);
+  
+  do {
+    randomNumber3 = Math.floor(Math.random() * Product.productsArray.length);
+  } while (randomNumber1 === randomNumber2 || randomNumber1 === randomNumber3 || randomNumber3 === pastRandomNumber2 || randomNumber3 === pastRandomNumber3);
+
+  product1.src = Product.productsArray[randomNumber1].src;
+  product2.src = Product.productsArray[randomNumber2].src;
+  product3.src = Product.productsArray[randomNumber3].src;
+
+  product1.dataset.index = randomNumber1;
+  product2.dataset.index = randomNumber2;
+  product3.dataset.index = randomNumber3;
+  //saving the new random numbers into the past random numbers- only checks in the while loop
+  pastRandomNumber1 = randomNumber1;
+  pastRandomNumber2 = randomNumber2;
+  pastRandomNumber3 = randomNumber3;
+
+  //why did we do this, what is this doing???
+  prodName1.textContent = Product.productsArray[randomNumber1].imageName;
+  prodName2.textContent = Product.productsArray[randomNumber2].imageName;
+  prodName3.textContent = Product.productsArray[randomNumber3].imageName;
 };
-itemImage1.src = this.imageArray[randomNumber1].src;
-itemImage1.src = ItemImage.imageArray[randomNumber1].src;
-itemImage2.src = ItemImage.imageArray[randomNumber2].src;
-itemImage3.src = ItemImage.imageArray[randomNumber3].src;
+
+// product1.src = this.productsArray[randomNumber1].src;
+
+
 
 //new products
-new ItemImage('./img/bag.jpg', 'Star Wars Carry-ons');
-new ItemImage('./img/banana.jpg', 'Banana Slicer');
-new ItemImage('./img/bathroom.jpg', 'TP Tablet Holder');
-new ItemImage('./img/boots.jpg', 'Open Toed Rainboots');
-new ItemImage('./img/breakfast.jpg', 'All in One Breakfast Cooker');
-// new ItemImage(.)
+new Product('./img/bag.jpg', 'Star Wars Carry-ons');
+new Product('./img/banana.jpg', 'Banana Slicer');
+new Product('./img/bathroom.jpg', 'TP Tablet Holder');
+new Product('./img/boots.jpg', 'Open Toed Rainboots');
+new Product('./img/breakfast.jpg', 'All in One Breakfast Cooker');
+new Product('./img/bubblegum.jpg', 'Meatball Bubblegum');
 
-ItemImage.renderThreeRandomly();
+
+function handleClickEvent(event) {
+  Product.renderThreeRandomly();
+  // var clickedNode = event.target;
+
+  // Product.votecount++;
+  // if (Product.voteCount === 25);
+  //stop counter and display
+}
+// Product.random();
+
+
+imageSection.addEventListener('click', handleClickEvent);
+
+Product.renderThreeRandomly();
